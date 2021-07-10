463bd8c1899d2fd5fe62a842f441824d85a0f365
Bhanuteja Aryasomayajula <teja275@gmail.com>
2021-07-10 13:30:21 +0530

    Folder structure from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python repository for the data hackathon conducted by TheMathCompany. The objective of the hackathon is to predict the prices of cars using data.',
+    author='Bhanuteja Aryasomayajula',
+    license='MIT',
+)
